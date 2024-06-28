f7f40bc88e3d153ef4acd5986934d4d42b4ea214
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-06-29 02:12:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ let ig = window.Instagram.WebApp;
 
 ig.expand();
 
-let params = (new URL(document.location)).searchParams;
-console.log(params.get("fcbsid");
-
 let btnclose = document.getElementById("btn3");
 
 btnclose.addEventListener("click", function(){
